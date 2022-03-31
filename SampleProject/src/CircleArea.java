e21d4f3e18fff701ece44590f97af4323cb37abd
HyungKwon Lee <lhk6565@naver.com>
2022-03-31 18:04:52 +0900

    study java

---
+++ SampleProject/src/CircleArea.java
@@ -0,0 +1,13 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		final double Pi = 3.14;
+		double radius = 10.0;
+		double circleArea = radius*radius*Pi;
+		double circleArealength = radius*2*Pi;
+		System.out.println("원의 면적 = " + circleArea);
+		System.out.println("원의 둘레 = " + circleArealength);
+	}
+
+}
