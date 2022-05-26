dc6b7133ae7a4fcb6a006274a5176bdefb76067c
HyungKwon Lee <lhk6565@naver.com>
2022-05-26 17:18:32 +0900

    java study

---
+++ SampleProject/src/DivideByZero.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int dividend;
+		int divisor;
+		
+		System.out.println("나뉨수를 입력하세요.");
+		dividend = scanner.nextInt();
+		System.out.println("나눗수를 입력하세요.");
+		divisor = scanner.nextInt();
+		System.out.println(dividend + "를 " + divisor + "로 나누면 몫은 " + dividend/divisor + "입니다.");
+		scanner.close();
+	}
+
+}
